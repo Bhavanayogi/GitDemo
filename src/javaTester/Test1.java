f0486b75a8e49baecbd156c9b367df06693a992a
Guddu <bhavanayogi5@gmail.com>
2025-08-15 22:47:55 +0530

    American TimeZone

--- src/javaTester/Test1.java
+++ src/javaTester/Test1.java
@@ -1,7 +1,9 @@
 package javaTester;
 
 public class Test1 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("This is test1 class");
+        // This is a comment
+        System.out.println("making changes through the GitX IDE");
     }
 }
