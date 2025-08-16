8ac83106738114ff3e7eb650403e6ce4d2439ed1
Guddu <bhavanayogi5@gmail.com>
2025-08-16 12:40:46 +0530

    final commit demo class

---
+++ src/javaTester/demo.java
@@ -0,0 +1,10 @@
+package javaTester;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("This is a demo class");
+        System.out.println("Demonstrating basic Java functionality");
+        System.out.println("Adding more functionality to the demo class");
+        System.out.println("End of demo class");
+    }
+}
